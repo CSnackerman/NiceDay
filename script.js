a83ec266dc8e9d34ee0224a4b034879baf0ade50
Colby <colbyackerman@gmail.com>
2021-09-30 13:51:36 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext ('2d');
+
+// width and height of canvas
+const WIDTH = 500;
+const HEIGHT = 500;
+
+const DIRT_HEIGHT = 50;
+const GRASS_SIZE = 50;
+
+
+// load grass images
+const grass1 = new Image();
+grass1.src = 'images/grass1.png'
+
+const grass2 = new Image();
+grass2.src = 'images/grass2.png'
+
+const grass3 = new Image();
+grass3.src = 'images/grass3.png'
+
+const grass4 = new Image();
+grass4.src = 'images/grass4.png'
+
+// test random grass selector
+console.log ('A random grass', selectRandomGrass())
+
+
+var grasses = [];
+
+
+// rendering function
+function main() {
+
+    // clear
+    ctx.fillStyle = 'skyblue';
+    ctx.fillRect (0, 0, WIDTH, HEIGHT);
+
+    // draw objects
+    drawDirt (ctx);
+    plantGrass (ctx);
+
+    
+    
+}
+
+// get the ball rolling
+main();
+
+// updates the canvas every 1 sec
+setInterval (() => {
+    requestAnimationFrame (main);
+}, 1000);
+
+// ================================================================ //
+
+// drawing functions
+
+function drawDirt (ctx) {
+    ctx.fillStyle = 'rgb(43, 20, 0)';
+    ctx.fillRect (0, HEIGHT - DIRT_HEIGHT, WIDTH, DIRT_HEIGHT);
+}
+
+
+function plantGrass (ctx) {
+
+    let numberOfGrasses = 100;
+
+    for (let i = 0; i < numberOfGrasses; i++) {
+        let grass = selectRandomGrass();
+        let x = randomRange (0, WIDTH);
+        let y = randomRange (HEIGHT - DIRT_HEIGHT - GRASS_SIZE, HEIGHT - GRASS_SIZE)
+        ctx.drawImage (grass, x, y, GRASS_SIZE, GRASS_SIZE);
+    }
+    
+}
+
+// utility functions
+
+function selectRandomGrass() {
+
+    // select a random number between 0 and 3
+    let selection = randomRange (0, 3);
+
+    if (selection === 0) return grass1;
+    if (selection === 1) return grass2;
+    if (selection === 2) return grass3;
+    if (selection === 3) return grass4;
+}
+
+
+function randomRange (min, max) {
+    min = Math.ceil (min);
+    max = Math.floor (max) + 1;
+    return Math.floor (Math.random() * (max - min) + min);
+}
